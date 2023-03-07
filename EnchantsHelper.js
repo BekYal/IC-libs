7bdf3a2b1f30406bb4ca56f88df91a8f3a9fce68
hygohy@gmail.com <hygohy@gmail.com>
2023-03-07 18:28:40 +0300

    fix registerHurtFunctions

--- EnchantsHelper.js
+++ EnchantsHelper.js
@@ -1,13 +1,13 @@
 const Enchants = {
-	registerHurtFunction: function(enchant, func, level) {
+	registerHurtFunction: function(enchant, func, lev) {
 Callback.addCallback('EntityHurt',
 	function (attacker, victim, damageValue, damageType, someBool1, someBool2) {
 		let item = Entity.getCarriedItem(attacker);
 		let actor = new PlayerActor(attacker);
-		if (item.extra && item.extra.getEnchantLevel(enchant) == (level || 1) ) {
-			
-			func(item, attacker, victim, damageValue, damageType, someBool1, someBool2);
+		if (item.extra && item.extra.getEnchantLevel(enchant) == (lev || 1) ) {
+			let enchantLevel = item.extra.getEnchantLevel(enchant);
+			func(item, enchantLevel, attacker, victim, damageValue, damageType);
 		}
 	});
 	}
-};
+}
