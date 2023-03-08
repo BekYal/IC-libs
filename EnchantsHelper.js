1adcebe7c642f12a61915f48025aa1e87ff1ec79
hygohy@gmail.com <hygohy@gmail.com>
2023-03-08 17:40:03 +0300

    add use function

--- EnchantsHelper.js
+++ EnchantsHelper.js
@@ -3,8 +3,8 @@ const Enchants = {
 	hurtFunction: function(enchant, func, level) {
 		Callback.addCallback('EntityHurt', function(attacker, victim, damageValue, damageType, someBool1, someBool2) {
 			let item = Entity.getCarriedItem(attacker);
-			let enchantLevel = item.extra.getEnchantLevel(enchant);
 			if (item.extra && item.extra.getEnchantLevel(enchant) == (level || 1) && damageType == 2) {
+				let enchantLevel = item.extra.getEnchantLevel(enchant);
 				func(item, enchantLevel, attacker, victim, damageValue, damageType);
 			}
 		});
@@ -13,11 +13,35 @@ const Enchants = {
 	destroyBlock: function(enchant, func, level) {
 		Callback.addCallback('DestroyBlock', function(coords, block, player) {
 			let item = Entity.getCarriedItem(player);
-			let enchantLevel = item.extra.getEnchantLevel(enchant);
 			if (item.extra && item.extra.getEnchantLevel(enchant) == (level || 1)) {
+				let enchantLevel = item.extra.getEnchantLevel(enchant);
+
 				func(item, enchantLevel, coords, block, player);
+			}
+		});
+	},
+	useFunction: function(enchant, func, level) {
+		Callback.addCallback("ItemUse", function(coords, item, block, isExternal, player) {
+			if (item.extra && item.extra.getEnchantLevel(enchant) == (level || 1)) {
+				let enchantLevel = item.extra.getEnchantLevel(enchant);
+				func(coords, item, block, isExternal, player, enchantLevel);
 			}
 		});
 	}
 
 };
+
+let LiveSteal = CustomEnchant.newEnchant("LiveSteal", Translation.translate("LiveSteal"))
+	.setMinMaxLevel(1, 3)
+	.setMask(MASKS.axe)
+	.setFrequency(1);
+Enchants.useFunction(LiveSteal.id, function(coords, item, block, isExternal, player, enchantLevel) {
+	Game.message(item.extra + "");
+}, 3);
+Enchants.destroyBlock(LiveSteal.id, function(item, enchantLevel, coords, block, player) {
+	Game.message(item.extra + "");
+}, 3);
+
+Enchants.hurtFunction(LiveSteal.id, function(item, enchantLevel, attacker, victim, damageValue, damageType) {
+	Game.message(item.extra + "");
+}, 3);
