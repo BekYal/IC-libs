3a44c07974fe59f78a47b3d0cde14a8ea6462f0a
hygohy@gmail.com <hygohy@gmail.com>
2023-04-26 20:22:32 +0300

    edit some poop code

--- InvUtil/InvUtil.js
+++ InvUtil/InvUtil.js
@@ -1,82 +1,78 @@
 var invUtil = {
-	getItemCount: function(id, data, extra) {
-		for (let y = 0; y <= 40; y++) {
-			let actor = new PlayerActor(Player.get());
-			let item = actor.getInventorySlot(y);
-			if (item.id == id && item.data == data && item.extra)
-				if (extra) {
-					if (item.extra == extra) {
-						return item.count;
-					}
-				} else {
-					return item.count;
-				}
-		}
-	},
+    getItemCount: function(id, data, extra) {
+        for (let y = 0; y <= 40; y++) {
+            let actor = new PlayerActor(Player.get());
+            let item = actor.getInventorySlot(y);
+            if (item.id == id && item.data == data && item.extra)
+                if (extra) {
+                    if (item.extra == extra) {
+                        return item.count;
+                    }
+                } else {
+                    return item.count;
+                }
+        }
+    },
+    dropInventoryItem: function(id, count, data, extra) {
+        let player = Player.get(),
+            actor = new PlayerActor(player),
+            pos = Entity.getPosition(player);
+        for (let y = 0; y <= 40; y++) {
+            let item = actor.getInventorySlot(y);
+            if (item.id == id && item.data == data && item.extra && item.extra == (extra || item.extra) && item.count == count) {
+                World.drop(pos.x, pos.y, pos.z, id, count, data, item.extra);
 
-	dropInventoryItem: function(id, count, data, extra) {
-		for (let y = 0; y <= 40; y++) {
-			let actor = new PlayerActor(Player.get());
-			let item = actor.getInventorySlot(y);
-			let pos = Entity.getPosition(Player.get());
-			if (item.id == id && item.data == data && item.extra && item.extra == (extra || item.extra) && item.count == count) {
-				World.drop(pos.x, pos.y, pos.z, id, count, data, item.extra);
-			}
-		}
-	},
+            }
+        }
+    },
 
-	hasItem: function(slot) {
-		let actor = new PlayerActor(Player.get());
-		let item = actor.getInventorySlot(y);
-		if (item.id && item.data) {
-			return true;
-		} else {
-			return false;
-		}
-	},
-	//хз робит ли
-	getLeftHand: function() {
-		return Entity.getOffhandItem(Player.get());
-	},
+    hasItem: function(slot) {
+        let actor = new PlayerActor(Player.get());
+        let item = actor.getInventorySlot(y);
+        if (item.id && item.data) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    //хз робит ли
+    getLeftHand: function() {
+        return Entity.getOffhandItem(Player.get());
+    },
 
-	removeItem: function(id, data, extra, decrease) {
-		for (let y = 0; y <= 36; y++) {
-			let actor = new PlayerActor(Player.get());
-			let item = actor.getInventorySlot(y);
-			if (item.id == id && item.data == data && item.extra && item.extra == extra) {
-				actor.setInventorySlot(y, item.id, item.count - decrease || 1, item.data, item.extra)
-			}
-		}
-	},
-	removeingItem: function(id, data, decrease) {
-		Callback.addCallback("ServerPlayerTick", function(player) {
-			for (let y = 0; y <= 36; y++) {
-				let actor = new PlayerActor(Player.get());
-				let item = actor.getInventorySlot(y);
-				if (item.id == id && item.data == data) {
-					actor.setInventorySlot(y, item.id, item.count - decrease || 1, item.data, item.extra)
-				}
-			}
-		});
-	},
+    removeItem: function(id, data, extra, decrease) {
+        for (let y = 0; y <= 36; y++) {
+            let actor = new PlayerActor(Player.get());
+            let item = actor.getInventorySlot(y);
+            if (item.id == id && item.data == data && item.extra && item.extra == extra) {
+                actor.setInventorySlot(y, item.id, item.count - decrease || 1, item.data, item.extra)
+            }
+        }
+    },
+    removeingItem: function(id, data, decrease) {
+        Callback.addCallback("ServerPlayerTick", function(player) {
+            for (let y = 0; y <= 36; y++) {
+                let actor = new PlayerActor(Player.get());
+                let item = actor.getInventorySlot(y);
+                if (item.id == id && item.data == data) {
+                    actor.setInventorySlot(y, item.id, item.count - decrease || 1, item.data, item.extra)
+                }
+            }
+        });
+    },
 
-	getInventorySize: function() {
-		return 36;
-	},
+    getInventorySize: function() {
+        return 36;
+    },
 
-	getItemSlot: function(id, data, extra) {
-		for (let y = 0; y <= 40; y++) {
-			let actor = new PlayerActor(Player.get());
-			let item = actor.getInventorySlot(y);
-			if (item.id == id && item.data == data && item.extra)
-				if (extra) {
-					if (item.extra == extra) {
-						return y;
-					}
-				} else {
-					return y;
-				}
-		}
-	}
+    getItemSlot: function(id, data, extra) {
+        let actor = new PlayerActor(Player.get());
+        for (let s = 0; s < 36; s++) {
+            let item = actor.getInventorySlot(s);
+            if (item.extra && item.extra == extra && item.id == id && item.data == data) {
+                return item;
+            }
+        }
+    }
 
 };
